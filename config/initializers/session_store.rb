5e870b7a33155a13fecf2caf56ea34cce565ddd7
Dinshaw Gobhai <info@pcgi.it>
2009-03-15 00:09:48 -0400

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,2 @@
+ActionController::Base.session = { :session_key => '_KYVNLI_session', :secret => 'QUNECPFMRSIHOAWBYXDXXMGKPZZUHDSJSPIOKWEL' }
+ActionController::Base.session_store = :active_record_store
